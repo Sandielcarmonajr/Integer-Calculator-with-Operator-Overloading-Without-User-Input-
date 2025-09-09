5d38f99b0cfa02ed0f3dda394b3f1f8f85f3db2e
Sandielcarmonajr <114954027+Sandielcarmonajr@users.noreply.github.com>
2025-09-09 15:10:26 -0400

    Add files via upload

---
+++ Integer.h
@@ -0,0 +1,21 @@
+//Integer.h
+
+class Integer{
+    private:
+      int value;
+    
+    public:
+      Integer();
+      
+      Integer(int val);
+      
+      int getValue() const;
+      
+      void setValue(int val);
+      
+      Integer operator+(const Integer& other) const;
+      Integer operator-(const Integer& other) const;
+      Integer operator*(const Integer& other) const;
+      Integer operator/(const Integer& other) const;
+    
+};
